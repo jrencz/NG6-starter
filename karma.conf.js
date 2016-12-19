f48835d61457921713fd70e2bbf6a2302bf64053
Jarek Rencz <jarek.rencz@functionite.com>
2016-12-19 18:38:30 +0100

    Longest specs reporter

--- karma.conf.js
+++ karma.conf.js
@@ -15,6 +15,7 @@ module.exports = function (config) {
 
     plugins: [
       require("karma-chai"),
+      require("karma-longest-reporter"),
       require("karma-chrome-launcher"),
       require("karma-mocha"),
       require("karma-mocha-reporter"),
@@ -49,7 +50,10 @@ module.exports = function (config) {
     },
 
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['mocha'],
+    reporters: [
+      'mocha',
+      'longest',
+    ],
 
     // web server port
     port: 9876,
