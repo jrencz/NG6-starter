d9cbaa8844fc75b1392dd9608f42f6050f9f1a17
Jarek Rencz <jarek.rencz@functionite.com>
2016-12-20 14:21:04 +0100

    Move to gulp 4 prerelease (local) + update Gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,12 +3,9 @@
 import gulp     from 'gulp';
 import webpack  from 'webpack';
 import path     from 'path';
-import sync     from 'run-sequence';
 import rename   from 'gulp-rename';
 import template from 'gulp-template';
-import fs       from 'fs';
 import yargs    from 'yargs';
-import lodash   from 'lodash';
 import gutil    from 'gulp-util';
 import serve    from 'browser-sync';
 import del      from 'del';
@@ -17,19 +14,19 @@ import webpackHotMiddleware from 'webpack-hot-middleware';
 import colorsSupported      from 'supports-color';
 import historyApiFallback   from 'connect-history-api-fallback';
 
-let root = 'client';
+const root = 'client';
 
 // helper method for resolving paths
-let resolveToApp = (glob = '') => {
+const resolveToApp = (glob = '') => {
   return path.join(root, 'app', glob); // app/{glob}
 };
 
-let resolveToComponents = (glob = '') => {
+const resolveToComponents = (glob = '') => {
   return path.join(root, 'app/components', glob); // app/components/{glob}
 };
 
 // map of all paths
-let paths = {
+const paths = {
   js: resolveToComponents('**/*!(.spec.js).js'), // exclude spec files
   scss: resolveToApp('**/*.scss'), // stylesheets
   html: [
@@ -45,25 +42,34 @@ let paths = {
   dest: path.join(__dirname, 'dist')
 };
 
+gulp.task('clean', () => del([paths.dest])
+  .then((paths) => {
+    gutil.log("[clean]", paths);
+  })
+);
+
 // use webpack.config.js to build modules
-gulp.task('webpack', ['clean'], (cb) => {
-  const config = require('./webpack.dist.config');
-  config.entry.app = paths.entry;
+gulp.task('webpack', gulp.series(
+  'clean',
+  (cb) => {
+    const config = require('./webpack.dist.config');
+    config.entry.app = paths.entry;
 
-  webpack(config, (err, stats) => {
-    if(err)  {
-      throw new gutil.PluginError("webpack", err);
-    }
+    webpack(config, (err, stats) => {
+      if (err) {
+        throw new gutil.PluginError("webpack", err);
+      }
 
-    gutil.log("[webpack]", stats.toString({
-      colors: colorsSupported,
-      chunks: false,
-      errorDetails: true
-    }));
+      gutil.log("[webpack]", stats.toString({
+        colors: colorsSupported,
+        chunks: false,
+        errorDetails: true
+      }));
 
-    cb();
-  });
-});
+      cb();
+    });
+  })
+);
 
 gulp.task('serve', () => {
   const config = require('./webpack.dev.config');
@@ -74,7 +80,7 @@ gulp.task('serve', () => {
     // application entry point
   ].concat(paths.entry);
 
-  var compiler = webpack(config);
+  const compiler = webpack(config);
 
   serve({
     port: process.env.PORT || 3000,
@@ -95,7 +101,7 @@ gulp.task('serve', () => {
   });
 });
 
-gulp.task('watch', ['serve']);
+gulp.task('watch', gulp.series('serve'));
 
 gulp.task('component', () => {
   const cap = (val) => {
@@ -116,11 +122,4 @@ gulp.task('component', () => {
     .pipe(gulp.dest(destPath));
 });
 
-gulp.task('clean', (cb) => {
-  del([paths.dest]).then(function (paths) {
-    gutil.log("[clean]", paths);
-    cb();
-  })
-});
-
-gulp.task('default', ['watch']);
+gulp.task('default', gulp.series('watch'));
